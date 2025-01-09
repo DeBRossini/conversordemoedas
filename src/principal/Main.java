a3d0904b182d53afaa766288b4475f27021a2664
Débora Rossini <dehrossini@outlook.com>
2025-01-09 18:38:09 -0300

    Final

---
+++ src/principal/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+import menu.Menu;
+
+    public class Principal {
+        public static void main(String[] args) {
+            Menu mostraMenu = new Menu();
+            mostraMenu.displayMenu();
+        }
+    }
